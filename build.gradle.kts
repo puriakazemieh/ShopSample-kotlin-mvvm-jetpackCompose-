1736d598726040f78dfbdb59a12a7ece8f6b2975
puriakazemieh <puriakazemieh@gmail.com>
2023-11-14 10:53:23 +0330

    [LIBRARY] Add plugins for dependency injection - hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
