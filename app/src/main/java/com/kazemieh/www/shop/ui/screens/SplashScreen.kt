b39d2dd3f11bfb393ae0ead8e5890d233bc2a973
puriakazemieh <puriakazemieh@gmail.com>
2023-11-14 13:12:36 +0330

    [STYLE] Create Splash Screen and Add logo and loading

--- app/src/main/java/com/kazemieh/www/shop/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/kazemieh/www/shop/ui/screens/SplashScreen.kt
@@ -1,10 +1,67 @@
 package com.kazemieh.www.shop.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.kazemieh.www.shop.R
+import com.kazemieh.www.shop.navigation.Screen
+import com.kazemieh.www.shop.ui.component.Loading3Dots
+import com.kazemieh.www.shop.ui.theme.SplashBg
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Text(text = "SplashScreen")
+    Splash()
+    LaunchedEffect(key1 = true) {
+        delay(2000)
+        navController.navigate(Screen.Home.route)
+    }
+}
+
+@Composable
+fun Splash() {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.SplashBg)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.digi_logo),
+            contentDescription = null,
+            modifier = Modifier.size(250.dp)
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(100.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.digi_txt_white),
+                contentDescription = null,
+                modifier = Modifier.size(75.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Loading3Dots(isDark = false)
+        }
+    }
 }
