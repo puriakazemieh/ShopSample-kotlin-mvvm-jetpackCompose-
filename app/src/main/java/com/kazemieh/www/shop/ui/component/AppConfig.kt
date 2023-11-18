ccf580593313e59c1a3cac1575c4a58dadb381db
puriakazemieh <puriakazemieh@gmail.com>
2023-11-18 12:36:54 +0330

    [STYLE] Show language and direction in ProfileScreen with Button

--- app/src/main/java/com/kazemieh/www/shop/ui/component/AppConfig.kt
+++ app/src/main/java/com/kazemieh/www/shop/ui/component/AppConfig.kt
@@ -1,7 +1,6 @@
 package com.kazemieh.www.shop.ui.component
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.kazemieh.www.shop.util.Constants.USER_LANGUAGE
 import com.kazemieh.www.shop.viewmodel.DataStoreViewModel
